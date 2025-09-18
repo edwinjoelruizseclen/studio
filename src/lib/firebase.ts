17fccb14e516b193f7dfe5239e2ed89158aa4ba2
Edwin RS <rsedwin06@gmail.com>
2025-09-18 21:36:11 +0000

    igual no puedo registrarme

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,6 +5,12 @@ import { firebaseConfig } from './firebase-config';
 // Initialize Firebase
 const firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-const auth = getAuth(firebaseApp);
+const auth = getAuth(firebaseApp, {
+  persistence: undefined,
+  initializeWithValue: {
+    authDomain: firebaseConfig.authDomain,
+  }
+});
+
 
 export { auth, firebaseApp };
