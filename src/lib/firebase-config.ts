c3e40587670e2aa0f54ce45314239284219d2f9f
Edwin RS <rsedwin06@gmail.com>
2025-09-18 21:05:40 +0000

    File changes

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,10 +1,10 @@
 // @TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  projectId: 'studio-5081739751-18ca0',
+  appId: '1:1019606117722:web:f21ad6d90748eca1195763',
+  apiKey: 'AIzaSyAX_i39TqOBQrJpl_D4q8iCkEYt4wokDSU',
+  authDomain: 'studio-5081739751-18ca0.firebaseapp.com',
+  storageBucket: 'studio-5081739751-18ca0.appspot.com',
+  messagingSenderId: '1019606117722',
 };
